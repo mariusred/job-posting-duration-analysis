cb2b3bac6c5b3e7aec932a1ce8a259f858315037
Marius Rollon <mariusrollon@gmail.com>
2025-05-08 00:55:24 +0800

    ♻️ Refactored scraping code
    Modularized the program flow for readability and cleaner structure.
    Improved documentation in doctrings

--- Jobstreet_scraper.py
+++ Jobstreet_scraper.py
@@ -1,262 +1,246 @@
+import time
+import csv
+import os
+import math
+
 from selenium import webdriver
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 from bs4 import BeautifulSoup
-import time
-import csv
-import os
-import math
 
 # Set up Edge options
 options = Options()
 service = Service(log_pathos=os.devnull)
 options.use_chromium = True
-#options.add_argument("--headless")
-options.add_argument("--disable-gpu")
+#options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument('--log-level=3')
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
 current_time = time.localtime()
-formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", current_time)
+formatted_time = time.strftime('%Y-%m-%d %H:%M:%S', current_time)
 
 # Point to Edge driver (skip path if it's in PATH)
 driver = webdriver.Edge(service=service, options=options)
 
-# Load JobStreet page
-url = "https://ph.jobstreet.com/junior-data-scientist-jobs/in-Metro-Manila?sortmode=ListedDate"
-url_parts = url.partition('?')
+# Load JobStreet page and divide it into sections that will be concatenated when accessing other pages
+url = 'https://ph.jobstreet.com/junior-data-scientist-jobs/in-Metro-Manila?sortmode=ListedDate'
 
-driver.get(url)
- 
-#Let the webpage load and look for job posts in article tag. If none, initiate timeout.  
-try:
-    WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "article[data-automation='normalJob']"))
-    )
-except:
-    print("Job listings did not load in time.")
-    time.sleep(5)
-    driver.quit()
-    exit()
 
 filename = 'jobstreet_jobs.csv'
+
 job_data = []
+is_file_exiting = os.path.exists(filename)
 
-# Parse page content
-soup = BeautifulSoup(driver.page_source, 'html.parser')
+def wait_for_website(web_url):
+    """ Waits for a website to load contents that are classified as normal jobs.
 
-job_cards = soup.find_all('article', attrs={'data-automation': 'normalJob'})
-job_total = soup.find('span', attrs={'data-automation': 'totalJobsCount'}).text.strip()
-search_pages = math.ceil(int(job_total) / len(job_cards))
-#Verify amount of job posts found
-print(f"Got {int(job_total)} job listings across {search_pages} pages. Displaying {len(job_cards)} Results..")
-print(f'Number of jobs in page 1: {len(job_cards)}')
+    Args:
+        web_url: string of website url to load.
+    Returns:
+       None
+    """
+    driver.get(web_url)  
+    try:
+        WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "article[data-automation='normalJob']"))
+        )
+    except:
+        print('Job listings did not load in time.')
+        time.sleep(5)
+        driver.quit()
+        return
 
-def extract_job_data(job):
-    job_title = job.find('a', attrs={'data-automation': 'jobTitle'}).text.strip()
+def parse_page_contents(web_url, first_page = True):
+    """ Parses website contents for all job postings.
+       
+        When used on the first page of a search result url, 
+        include total search results and number of search pages.
+       
+        When used on succeeding pages, return job postings only.
+
+    Args:
+        web_url: string of website url to load.
+        first_page: Boolean for separting return values of first web search
+                    and next pages.
+    Returns:
+        job_cards: iterable query result for all job postings in a page
+        job_total: integer total amount of jobs found
+        search_pages: integer number of search pages for total amount of jobs
+    """
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    job_cards = soup.find_all('article', attrs={'data-automation': 'normalJob'})
     
-    job_company = job.find('a', attrs={'data-automation': 'jobCompany'})
-    if not job_company:
-        job_company = job.find('span', attrs={'data-automation': 'jobCompany'})
-    if job_company:
-        company = job_company.text.strip()
-    else:
-        company = 'N/A'
+    if first_page:
+        job_total = soup.find('span', attrs={'data-automation': 'totalJobsCount'}).text.strip()
+        search_pages = math.ceil((int(job_total) - 4) / len(job_cards))
     
-    job_location_spans = job.find_all('span', attrs={'data-automation': 'jobLocation'})
-    location_parts = []
-    for span in job_location_spans:
-        location_parts.append(span.text.strip())
-    location = ', '.join(location_parts)
-    
-    job_classification = job.find('span', attrs={'data-automation':'jobSubClassification'})
-    if not job_classification:
-        job_classification = 'N/A'
-    else:
-        job_classification = job_classification.text.strip()
-    
-    job_industry = job.find('span', attrs={'data-automation':'jobClassification'})
-    if not job_industry:
-        job_industry = 'N/A'
-    else:
-        job_industry = job_industry.text.strip()[1:-1]
-    
-    job_salary = job.find('span', attrs={'data-automation':'jobSalary'})
-    if not job_salary:
-        job_salary = 'N/A'
-    else:
-        job_salary = job_salary.text.strip()
-    
-    job_setup = job.find('span', attrs={'data-testid':'work-arrangement'})
-    if not job_setup:
-        job_setup = 'N/A'
-    else:
-        job_setup = job_setup.text.strip()[1:-1]
+        #Verify amount of job posts found
+        print(f'Got {int(job_total) - 4} job listings across {search_pages} pages. Displaying {len(job_cards)} Results..')
+        print(f'Number of jobs in this page: {len(job_cards)}')
         
-    job_listing_date = job.find('span', attrs={'data-automation': 'jobListingDate'}).text.strip()
+        return job_cards, job_total, search_pages
+    else:
+        return job_cards, None, None
     
-    job_link = job.find('a', attrs={'data-automation':'job-list-view-job-link'})
-    link_href = job_link.get('href')
-    job_url = "https://ph.jobstreet.com" + link_href if link_href else 'N/A'
-    
-    job_data_entry = job_data.append({
-        'Date Obtained': formatted_time,
-        'Link' : job_url,
-        'Company' : company,
-        'Position' : job_title,
-        'Location' : location,
-        'Classification' : job_classification,
-        'Industry' : job_industry,
-        'Salary' : job_salary,
-        'Work Arrangement' : job_setup,
-        'Day Posted' : job_listing_date
-    })
-    print(f'Current data rows: {len(job_data)}')
+def parse_multiple_pages(search_pages, web_url):
+    """ Parses website contents for all job postings in succeeding search pages.
+       
+        Partitions the url into segments to include the page number segment in url.
+        Loops through each page to collect job posts
+
+    Args:
+        search_pages: integer number of search pages for total amount of jobs
+        web_url: string of website url to load.
+    Returns:
+        job_cards: iterable query result for all job postings in a page
+        job_total: integer total amount of jobs found
+        search_pages: integer number of search pages for total amount of jobs
+    """
+
+    url_parts = web_url.partition('?')
+    for page in range(2, search_pages + 1):
+        next_page = f'{url_parts[0]}{url_parts[1]}page={str(page)}&{url_parts[2]}'
+        wait_for_website(next_page)
+        
+        job_cards, _, _ = parse_page_contents(next_page, first_page = False)
+        print(f'Number of jobs in page {page}: {len(job_cards)}')
+        
+        extract_job_data(job_cards)
+        time.sleep(5)
     return
 
-for job in job_cards:
-    extract_job_data(job)
+def extract_job_data(job_cards):
+    """ Parses website contents for all job postings in succeeding search pages.
+       
+        Partitions the url into segments to include the page number segment in url.
+        Loops through each page to collect job posts and stores them in a job data
+        dictionary.
+
+    Args:
+        job_cards: iterable query result for all job postings in a page
+    Returns:
+        None
+    """
+    for job in job_cards:
+        job_title = job.find('a', attrs={'data-automation': 'jobTitle'}).text.strip()
+        
+        #Some jobs lists the company name in <span> tags to indicate private advertisers.
+        #Otherwise, <a> tags contain company's name
+        job_company = job.find('a', attrs={'data-automation': 'jobCompany'})
+        if not job_company:
+            job_company = job.find('span', attrs={'data-automation': 'jobCompany'})
+        if job_company:
+            company = job_company.text.strip()
+        else:
+            company = 'N/A'
+        
+        #Location is divided into two parts, name and region, under <span> tag   
+        job_location_spans = job.find_all('span', attrs={'data-automation': 'jobLocation'})
+        location_parts = []
+        for span in job_location_spans:
+            location_parts.append(span.text.strip())
+        location = ', '.join(location_parts)
+            
+        job_classification = job.find('span', attrs={'data-automation':'jobSubClassification'})
+        if not job_classification:
+            job_classification = 'N/A'
+        else:
+            job_classification = job_classification.text.strip()
+            
+        job_industry = job.find('span', attrs={'data-automation':'jobClassification'})
+        if not job_industry:
+            job_industry = 'N/A'
+        else:
+            job_industry = job_industry.text.strip()[1:-1]
+            
+        job_salary = job.find('span', attrs={'data-automation':'jobSalary'})
+        if not job_salary:
+            job_salary = 'N/A'
+        else:
+            job_salary = job_salary.text.strip()
+            
+        job_setup = job.find('span', attrs={'data-testid':'work-arrangement'})
+        if not job_setup:
+            job_setup = 'N/A'
+        else:
+            job_setup = job_setup.text.strip()[1:-1]
+                
+        job_listing_date = job.find('span', attrs={'data-automation': 'jobListingDate'}).text.strip()
+            
+        job_link = job.find('a', attrs={'data-automation':'job-list-view-job-link'})
+        link_href = job_link.get('href')
+        job_url = 'https://ph.jobstreet.com' + link_href if link_href else 'N/A'
+        
+        #Each job result is logged into a dictionary to be appended in list.    
+        job_data_entry = job_data.append({
+            'Date Obtained': formatted_time,
+            'Link' : job_url,
+            'Company' : company,
+            'Position' : job_title,
+            'Location' : location,
+            'Classification' : job_classification,
+            'Industry' : job_industry,
+            'Salary' : job_salary,
+            'Work Arrangement' : job_setup,
+            'Day Posted' : job_listing_date
+        })
+    return
+
+def create_csv (filename):
+    """ Creates a csv file to store results if filename does not exist
+
+        Job data key-value pairs are written in rows to log job post.
+
+    Args:
+        filename: string of csv name that will contain scraped job posts.
+    Returns:
+        None
+    """
+    print(f'No filename found. Creating {filename}...')
+    print(f'Attaching {len(job_data)} job listings on {filename}')
+    field_names = job_data[0].keys()
+    with open(filename, mode = 'w', newline = '', encoding = 'utf-8-sig') as file:
+            writer = csv.DictWriter(file, fieldnames = field_names)
+            writer.writeheader()
+            writer.writerows(job_data)
+
+def append_results_to_csv (filename):
+    """ Appends scraping results to an existing csv filename.
+
+        Acts as running reference of job posts for the project.
+
+    Args:
+        filename: string of csv name that will contain scraped job posts.
+    Returns:
+        None
+    """
+    print(f'Attaching {len(job_data)} job listings on {filename}')
+    field_names = job_data[0].keys()
+    with open(filename, mode = 'a', newline = '', encoding = 'utf-8-sig') as file:
+        writer = csv.DictWriter(file, fieldnames = field_names)
+        writer.writerows(job_data)
+        
+wait_for_website(url)   
+
+job_cards, job_total, search_pages = parse_page_contents(url)
+extract_job_data(job_cards)
 time.sleep(5)
 
 if search_pages > 1:
-    for page in range(2, search_pages + 1):
-        next_page = url_parts[0] + url_parts[1] + 'page=' + str(page) + '&' + url_parts[2]
-        driver.get(next_page)
- 
-        #Let the webpage load and look for job posts in article tag. If none, initiate timeout.  
-        try:
-            WebDriverWait(driver, 20).until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, "article[data-automation='normalJob']"))
-            )
-        except:
-            print("Job listings did not load in time.")
-            input("Press enter to continue")
-            driver.quit()
-            exit()
+    parse_multiple_pages(search_pages,url)
+    
+if not is_file_exiting:
+    create_csv(filename)
+else:        
+    append_results_to_csv(filename)
 
-        # Parse page content
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        job_cards = soup.find_all('article', attrs={'data-automation': 'normalJob'})
-        print(f'Number of jobs in page {page}: {len(job_cards)}')
-        for job in job_cards:
-            extract_job_data(job)
-        time.sleep(5)
-
-field_names = job_data[0].keys()
-with open(filename, mode = 'w', newline = '', encoding = 'utf-8-sig') as file:
-        writer = csv.DictWriter(file, fieldnames = field_names)
-        writer.writeheader()
-        writer.writerows(job_data)
-        
 print(f'Done saving {len(job_data)} job listings on {filename}')
 time.sleep(10)
 driver.quit()
 
-
-'''
-#Extract relevant information from each job post
-for job in job_cards:
-    #Name of the job opening
-    job_title = job.find('a', attrs={'data-automation': 'jobTitle'}).text.strip()
-    
-    #Company name. Some job posts have private advertisers which are under span tags
-    #the if statement checks for those instances
-    job_company = job.find('a', attrs={'data-automation': 'jobCompany'})
-    if not job_company:
-        job_company = job.find('span', attrs={'data-automation': 'jobCompany'})
-    if job_company:
-        company = job_company.text.strip()
-    else:
-        company = 'N/A'
-    
-    #Job locations are divided into two span tags under one span parent tag. 
-    #Loop on each tag to output one location data
-    job_location_spans = job.find_all('span', attrs={'data-automation': 'jobLocation'})
-    location_parts = []
-    for span in job_location_spans:
-        location_parts.append(span.text.strip())
-    location = ', '.join(location_parts)
-    
-    #Job role department
-    job_classification = job.find('span', attrs={'data-automation':'jobSubClassification'})
-    if not job_classification:
-        job_classification = 'N/A'
-    else:
-        job_classification = job_classification.text.strip()
-    
-    #Job role industry
-    job_industry = job.find('span', attrs={'data-automation':'jobClassification'})
-    if not job_industry:
-        job_industry = 'N/A'
-    else:
-        job_industry = job_industry.text.strip()[1:-1]
-    
-    #Monthly salary of job role (if specified)
-    job_salary = job.find('span', attrs={'data-automation':'jobSalary'})
-    if not job_salary:
-        job_salary = 'N/A'
-    else:
-        job_salary = job_salary.text.strip()
-    
-    #Indicates work arrangement if on-site, remote, or hybrid
-    job_setup = job.find('span', attrs={'data-testid':'work-arrangement'})
-    if not job_setup:
-        job_setup = 'N/A'
-    else:
-        job_setup = job_setup.text.strip()[1:-1]
-    
-    #Time posted relative to date of scraping
-    job_listing_date = job.find('span', attrs={'data-automation': 'jobListingDate'}).text.strip()
-    
-    #Parse the gathered href to jobstreet website. For later use in gathering required skills
-    job_link = job.find('a', attrs={'data-automation':'job-list-view-job-link'})
-    link_href = job_link.get('href')
-    job_url = "https://ph.jobstreet.com" + link_href if link_href else 'N/A'
-    
-    
-    driver.get(job_url)
- 
-    #Let the webpage load and look for job posts in article tag. If none, initiate timeout.  
-    try:
-        WebDriverWait(driver, 60).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "article[data-automation='normalJob']"))
-        )
-    except:
-        print("Job listings did not load in time.")
-        input("Press enter to continue")
-        driver.quit()
-        exit()
-  
-    # Parse page content
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    job_cards = soup.find_all('article', attrs={'data-automation': 'normalJob'})
-    
-    
-    print("Date Obtained:", formatted_time)
-    print(f"Link: {job_url}")
-    print(f"Company Name: {company}")
-    print(f"Position: {job_title}")
-    print(f"Location: {location}")
-    print(f"Classification: {job_classification}")
-    print(f"Industry: {job_industry}")
-    print(f"Salary: {job_salary}")
-    print(f"Work Arrangement: {job_setup}")
-    print(f"Listing date: {job_listing_date}")
-    print('')
-
-    #Get a key-value pair for each result and attach the resulting pair to the overall job data
-    job_data_entry = job_data.append({
-        'Date Obtained': formatted_time,
-        'Link' : job_url,
-        'Company' : company,
-        'Position' : job_title,
-        'Location' : location,
-        'Classification' : job_classification,
-        'Industry' : job_industry,
-        'Salary' : job_salary,
-        'Work Arrangement' : job_setup,
-        'Day Posted' : job_listing_date
-    })
-    print('')    
-'''   
 
